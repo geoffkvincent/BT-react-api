52ac10a8d565b18d57a627471d24ed7fc4ef6475
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-12 10:04:06 -0600

    api call works

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import axios from 'axios'
+import {Container, Card} from 'semantic-ui-react'
 
 class Home extends Component {
+  state = { characters: [] }
+
+  componentDidMount() {
+    axios.get('https://api.got.show/api/characters/')
+    .then( ({data: characters}) => this.setState({characters}) )
+  }
+
   render() {
+    const {characters} = this.state
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <Container>
+        <Header as='h1' textAlign='center'>Game of Thrones Characteres</Header>
+        {characters.map(c =>
+          <Card key={c._id}>{c.name}</Card>
+          )}
+      </Container>
     );
   }
 }
