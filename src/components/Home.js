0cd3e8e83fce67d4c50de37bfcbebb6d5b93eb88
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-12 10:31:52 -0600

    added locations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 import axios from 'axios'
-import {Container, Card} from 'semantic-ui-react'
+import {Container, Card, Grid} from 'semantic-ui-react'
 
 class Home extends Component {
   state = { characters: [] }
 
   componentDidMount() {
-    axios.get('https://api.got.show/api/characters/')
+    axios.get('https://api.got.show/api/plod')
     .then( ({data: characters}) => this.setState({characters}) )
   }
 
   render() {
     const {characters} = this.state
     return (
-      <Container>
-        <Header as='h1' textAlign='center'>Game of Thrones Characteres</Header>
-        {characters.map(c =>
-          <Card key={c._id}>{c.name}</Card>
+      <div>
+      <Header as='h1' textAlign='center'>Game of Thrones Characteres</Header>
+      <Grid >
+        
+          {characters.map(c =>       
+              <Card key={c._id}>
+              <Card.Header>{c.name}</Card.Header>
+              <Card.Meta>{c.pageRank}</Card.Meta>
+              <Card.Description>{c.house}</Card.Description>
+              </Card>       
           )}
-      </Container>
+      </Grid>
+      </div>
     );
   }
 }
---
+++ src/components/Locations.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import axios from 'axios'
+
+class Locations extends React.Component {
+  state={locations: []}
+
+  componentDidMount() {
+  axios.get()
+  }
+
+  render() {
+    return(
+      null
+    )
+  }
+}
+
+export default Locations
